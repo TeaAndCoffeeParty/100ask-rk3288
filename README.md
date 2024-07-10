b8802d64c146f961588337687a6732d6304c75df
Chen Tuo <chentuo@sprintray.cn>
2024-07-10 01:08:39 -0700

    add cross compile tool

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # 100ask-rk3288
+
+
+## prepare the cross compile tool
+
+```shell
+cd ./firefly-rk3288_tools
+xz -d gcc-linaro-6.2.1-2016.11-x86_64_arm-linux-gnueabihf.tar.xz
+tar -xvf gcc-linaro-6.2.1-2016.11-x86_64_arm-linux-gnueabihf.tar
+```
--- firefly-rk3288_tools/add-corsstools.sh
+++ firefly-rk3288_tools/add-corsstools.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-export PATH=/home/qt/workspace/100ask/100ask-crsootool/bin:$PATH
+pwd_dir=$(pwd)
+tool_dir=$pwd_dir/gcc-linaro-6.2.1-2016.11-x86_64_arm-linux-gnueabihf/bin
+echo $tool_dir
+#export PATH=/home/qt/workspace/100ask/100ask-crsootool/bin:$PATH
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabihf-
