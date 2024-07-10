a4df741dba4181c6f652546b868f67c0284f4aa0
Chen Tuo <chentuo@sprintray.cn>
2024-07-10 01:21:12 -0700

    change the script for compile tool

--- firefly-rk3288_tools/add-crosstools.sh
+++ firefly-rk3288_tools/add-crosstools.sh
@@ -2,7 +2,7 @@
 
 pwd_dir=$(pwd)
 tool_dir=$pwd_dir/gcc-linaro-6.2.1-2016.11-x86_64_arm-linux-gnueabihf/bin
-echo $tool_dir
-#export PATH=/home/qt/workspace/100ask/100ask-crsootool/bin:$PATH
+
+export PATH=$tool_dir:$PATH
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabihf-
